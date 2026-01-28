92d0c6be85fc55ecc1e1440e05483b5cc33dad96
Tushar Sharma <ts817479@gmail.com>
2026-01-29 01:32:27 +0530

    update

--- 03_js/16_MouseEvents/index.js
+++ 03_js/16_MouseEvents/index.js
@@ -1,20 +1,37 @@
 // eventListener = Listen for specific events to create interactive web pages
-// events: click, mouseover, mouseout
-// • addEventListener(event, callback);
-const mybox=document.getElementById("mybox");
-const mybutton=document.getElementById("myButton");
+// events: click, mouseover, mouseout, keydown, keyup
+// addEventListener(event, callback);
+
+const mybox = document.getElementById("mybox");
+const mybutton = document.getElementById("myButton");
+
 function changecolor(event){
-    mybox.style.backgroundColor="tomato";
-    mybox.textContent="Aaahh";
+    mybox.style.backgroundColor = "tomato";
+    mybox.textContent = "Aaahh";
 }
+
 function changeShade(event){
-    mybox.style.backgroundColor="yellow";
-    mybox.textContent="Don't do it";
+    mybox.style.backgroundColor = "yellow";
+    mybox.textContent = "Don't do it";
 }
+
 function changeOriginal(event){
-    mybox.style.backgroundColor="lightgreen";
-    mybox.textContent="Click me";
+    mybox.style.backgroundColor = "lightgreen";
+    mybox.textContent = "Click me";
 }
-mybutton.addEventListener("click",changecolor);
-mybutton.addEventListener("mouseover",changeShade);
-mybutton.addEventListener("mouseout",changeOriginal);
+
+// Mouse events
+mybutton.addEventListener("click", changecolor);
+mybutton.addEventListener("mouseover", changeShade);
+mybutton.addEventListener("mouseout", changeOriginal);
+
+// Keyboard events
+document.addEventListener("keydown", function(event){ //pressing key.
+    mybox.style.backgroundColor = "skyblue";
+    mybox.textContent = `Key Down: ${event.key}`;
+});
+
+document.addEventListener("keyup", function(event){ //releasing key.
+    mybox.style.backgroundColor = "lightgreen";
+    mybox.textContent = `Key Up: ${event.key}`;
+});
