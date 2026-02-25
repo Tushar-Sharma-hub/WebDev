f8bd9fb2e441cb323267be07fb7c3e3e15fcb907
Tushar Sharma <ts817479@gmail.com>
2026-02-25 21:33:18 +0530

    update

---
+++ 04_React/01_ViteReact/src/App.jsx
@@ -0,0 +1,13 @@
+import First from "./First"
+
+function App() {
+
+  return (
+    <>
+      <h1>Tushar Sharma</h1>
+      <First/>
+    </>
+  ) 
+}
+
+export default App
---
+++ 04_React/01_ViteReact/src/First.jsx
@@ -0,0 +1,6 @@
+function First(){
+    return(
+        <h2>I am learning React.</h2>
+    )
+}
+export default First;
