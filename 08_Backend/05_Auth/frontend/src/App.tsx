ec0a9cbcecaa4b2216589a832c086a55b0f2cc72
Tushar-Sharma-hub <ts817479@gmail.com>
2026-05-27 03:12:29 +0530

    up

--- 08_Backend/05_Auth/frontend/src/App.tsx
+++ 08_Backend/05_Auth/frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [isLogin, setIsLogin] = useState(false)
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-linear-to-br from-blue-50 to-indigo-100 flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-2xl p-8 w-full max-w-md">
         {isLogin ? (
           <Login onSwitchToSignUp={() => setIsLogin(false)} />
